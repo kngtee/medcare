0a65000d40cdcc33677340fc2a70ab1be975d5c3
kngtee <anthonyurah@gmail.com>
2023-10-11 00:04:04 +0100

    Doctors About

---
+++ src/pages/Doctors/DoctorAbout.jsx
@@ -0,0 +1,92 @@
+import { formulateDate } from "../../utils/formulateDate";
+
+const DoctorAbout = () => {
+  return (
+    <>
+      <div>
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2">
+          About of
+          <span className="text-irisBlueColor font-bold text-[24px] leading-9">
+            Tee Chuks
+          </span>
+        </h3>
+        <p className="text__para">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus et
+          obcaecati pariatur at quis error nihil reprehenderit ullam fugiat,
+          vero blanditiis iusto facilis, quidem nobis voluptatum fuga dolore?
+          Odit ipsa officiis, nostrum quam ratione fugiat dolore consequatur!
+          Quae, temporibus, quibusdam non saepe sint alias nam enim a impedit
+          odit tempore.
+        </p>
+      </div>
+
+      <div className="mt-12">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+          Education
+        </h3>
+
+        <ul className="pt-4 md:p-5">
+          <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
+            <div>
+              <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
+                {formulateDate("09-01-2014")} - {formulateDate("08-14-2016")}
+              </span>
+              <p className="text-[15px] leading-6 font-medium text-textColor">
+                PHD in Surgeon
+              </p>
+            </div>
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              New Apollo Hospital, New York
+            </p>
+          </li>
+          <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
+            <div>
+              <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
+                {formulateDate("12-14-2010")} - {formulateDate("11-14-2014")}
+              </span>
+              <p className="text-[15px] leading-6 font-medium text-textColor">
+                PHD in Surgeon
+              </p>
+            </div>
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              New Apollo Hospital, New York
+            </p>
+          </li>
+        </ul>
+      </div>
+
+      <div className="mt-12">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+          Experience
+        </h3>
+
+        <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+              {formulateDate("09-01-2014")} - {formulateDate("08-14-2016")}
+            </span>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Sr. Surgeon
+            </p>
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              New Apollo Hospital, New York
+            </p>
+          </li>
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+              {formulateDate("09-01-2014")} - {formulateDate("08-14-2016")}
+            </span>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Sr. Surgeon
+            </p>
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              New Apollo Hospital, New York
+            </p>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default DoctorAbout;
--- src/pages/Doctors/Doctors.jsx
+++ src/pages/Doctors/Doctors.jsx
@@ -1,5 +1,6 @@
-// import DoctorCard from "./../../components/Doctors/DoctorCard";
-// import { doctor } from "./../../assets/data/doctors";
+import DoctorCard from "./../../components/Doctors/DoctorCard";
+import { doctors } from "./../../assets/data/doctors";
+import Testimonial from "../../components/Testimonial/Testimonial";
 
 const Doctors = () => {
   return (
@@ -13,7 +14,34 @@ const Doctors = () => {
               className="py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor"
               placeholder="Search Doctor"
             />
+            <button className="btn mt-0 rounded-[0px] rounded-r-md">
+              Search
+            </button>
           </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 lg:gap-[30px] ">
+            {doctors.map((doctor) => (
+              <DoctorCard key={doctor.id} doctor={doctor} />
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">What our patient say</h2>
+            <p className="text__para text-center">
+              World-class care for everyone. Our health system offers unmatched,
+              expert health care.
+            </p>
+          </div>
+
+          <Testimonial />
         </div>
       </section>
     </>
