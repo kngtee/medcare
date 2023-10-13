ef4031cc3f9b806d4d70cfb2846ae5079e20ebfd
kngtee <anthonyurah@gmail.com>
2023-10-13 21:52:47 +0100

    Feedback Feature Done

--- src/pages/Doctors/DoctorDetails.jsx
+++ src/pages/Doctors/DoctorDetails.jsx
@@ -3,6 +3,7 @@ import doctorImg from "../../assets/images/doctor-img02.png";
 import starIcon from "../../assets/images/Star.png";
 import DoctorAbout from "./DoctorAbout";
 import Feedback from "./Feedback";
+import SidePanel from "./SidePanel";
 
 const DoctorDetails = () => {
   const [tab, setTab] = useState("about");
@@ -67,7 +68,9 @@ const DoctorDetails = () => {
             </div>
           </div>
 
-          <div className=""></div>
+          <div>
+            <SidePanel />
+          </div>
         </div>
       </div>
     </section>
